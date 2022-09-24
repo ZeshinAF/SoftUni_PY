77c700930920159b4a6bdc4767150a514c70b07b
Zeshin <rozziplays@gmail.com>
2022-09-24 11:08:07 +0300

    Optimized

--- Exercise Data Types and Variables/elevator.py
+++ Exercise Data Types and Variables/elevator.py
@@ -1,11 +1,6 @@
+import math
+
 people = int(input())
 capacity = int(input())
-c = 0
-while people > 0:
-    cour = people // capacity
-    if cour == 0:
-        c += 1
-    else:
-        c += cour
-    people -= capacity * c
+c = math.ceil(people / capacity)
 print(c)
